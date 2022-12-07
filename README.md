ee3f4e61349fa5f55009e96aefd48d744342d35e
menard17 <menardmanlapaz1@gmail.com>
2022-12-07 10:40:17 +0800

    wip

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-"# event" 
-"# event" 
-# event
-# event
+for the backend folder name is mysite
+for the fortend folder name is client
--- restapi/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Event(models.Model):
-    event_name = models.CharField(max_length=200)
-    event_start = models.DateTimeField()
-    event_end = models.DateTimeField()
